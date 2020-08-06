6440bf55faef096e6025a2d41d8fc8f7d8a8c217
Nilay Singh <nilay@modistabox.com>
2020-08-06 20:12:01 +0530

    Removed Unwanted files

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"regexp"
-	models "wordplay/Models"
-	"wordplay/Router"
 
-	"github.com/Sirupsen/logrus"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
@@ -15,19 +15,36 @@ var (
 )
 
 func main() {
-	models.ConnectDataBase() // new
-	logger := logrus.New()
-	logger.Formatter = &logrus.JSONFormatter{}
+	// models.ConnectDataBase() // new
+	// logger := logrus.New()
+	// logger.Formatter = &logrus.JSONFormatter{}
 
-	logger.SetOutput(os.Stdout)
-	file, err := os.OpenFile("gin.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	// logger.SetOutput(os.Stdout)
+	// file, err := os.OpenFile("gin.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	// if err != nil {
+	// 	logger.Fatal(err)
+	// }
+
+	// defer file.Close()
+	// logger.SetOutput(file)
+
+	// Router.Router(logger)
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("gin.log")
+	// if we os.Open returns an error then handle it
 	if err != nil {
-		logger.Fatal(err)
+		fmt.Println(err)
 	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
 
-	defer file.Close()
-	logger.SetOutput(file)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	Router.Router(logger)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	fmt.Println(result["users"])
 
 }
